c52c489855ec5d82f0ec32dfa0dfa089ea68cbb6
Yajush Haarsh Rambhojun <haarsh.rambhojun@gmail.com>
2020-04-30 22:54:58 +0400

    Added more text styles

--- lib/constants.dart
+++ lib/constants.dart
@@ -26,3 +26,18 @@ const kTitleTextStyle = TextStyle(
   fontSize: 50.0,
   fontWeight: FontWeight.bold,
 );
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF22DF78),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
