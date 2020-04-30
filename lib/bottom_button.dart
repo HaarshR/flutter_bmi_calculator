7b03390003ec4be01374162ffd57f0860d7272d6
Yajush Haarsh Rambhojun <haarsh.rambhojun@gmail.com>
2020-04-30 22:57:19 +0400

    Separated BottomButton component

---
+++ lib/bottom_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({@required this.label, @required this.onTap});
+
+  final Function onTap;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: this.onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            this.label,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: Theme.of(context).accentColor,
+        margin: EdgeInsets.only(top: 10.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
