49aa156b30edad32f48ccd996809b735fbc534f4
Yajush Haarsh Rambhojun <haarsh.rambhojun@gmail.com>
2020-04-30 18:57:50 +0400

    Wrapped Container with GestureDetector with an onPress function

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.color, this.child});
+  ReusableCard({@required this.color, this.child, this.onPress});
 
   final Color color; //0xFF1D1E33
   final Widget child;
+  final Function onPress;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: this.child,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: this.color,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: this.onPress,
+      child: Container(
+        child: this.child,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: this.color,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
