56d3a169493ea80b2c97a1b7a382c344f25ac8cf
Yajush Haarsh Rambhojun <haarsh.rambhojun@gmail.com>
2020-04-30 19:11:43 +0400

    Added constants

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 const iconSize = 80.0;
 const gapSize = 15.0;
-const fontSize = 18.0;
-const textColor = Color(0xFF8D8E98);
 
 class IconContent extends StatelessWidget {
   IconContent({@required this.icon, @required this.label});
@@ -25,10 +24,7 @@ class IconContent extends StatelessWidget {
         ),
         Text(
           this.label,
-          style: TextStyle(
-            fontSize: fontSize,
-            color: textColor,
-          ),
+          style: kLabelTextStyle,
         )
       ],
     );
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,11 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
-
-const bottomContainerHeight = 80.0;
-const bottomContainerColor = Color(0xFF008080);
-const activeCardColor = Color(0xFF1D1E33);
-const inactiveCardColor = Color(0xFF111328);
+import 'constants.dart';
 
 enum Gender { male, female }
 
@@ -38,8 +34,8 @@ class _InputPageState extends State<InputPage> {
                         });
                       },
                       color: selectedGender == Gender.male
-                          ? activeCardColor
-                          : inactiveCardColor,
+                          ? kActiveCardColor
+                          : kInactiveCardColor,
                       child: IconContent(
                         icon: FontAwesomeIcons.mars,
                         label: 'MALE',
@@ -54,8 +50,8 @@ class _InputPageState extends State<InputPage> {
                         });
                       },
                       color: selectedGender == Gender.female
-                          ? activeCardColor
-                          : inactiveCardColor,
+                          ? kActiveCardColor
+                          : kInactiveCardColor,
                       child: IconContent(
                         icon: FontAwesomeIcons.venus,
                         label: 'FEMALE',
@@ -67,7 +63,7 @@ class _InputPageState extends State<InputPage> {
             ),
             Expanded(
               child: ReusableCard(
-                color: activeCardColor,
+                color: kActiveCardColor,
               ),
             ),
             Expanded(
@@ -75,22 +71,22 @@ class _InputPageState extends State<InputPage> {
                 children: <Widget>[
                   Expanded(
                     child: ReusableCard(
-                      color: activeCardColor,
+                      color: kActiveCardColor,
                     ),
                   ),
                   Expanded(
                     child: ReusableCard(
-                      color: activeCardColor,
+                      color: kActiveCardColor,
                     ),
                   ),
                 ],
               ),
             ),
             Container(
-              color: bottomContainerColor,
+              color: kBottomContainerColor,
               margin: EdgeInsets.only(top: 10.0),
               width: double.infinity,
-              height: bottomContainerHeight,
+              height: kBottomContainerHeight,
             ),
           ],
         ));
